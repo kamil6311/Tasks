429e2708d8e029467ecf1ee95b3d586be77c4931
Kamil.ABDEL-KADER@etu.uca.fr <kamil.abdel-kader@etu.uca.fr>
2021-12-11 17:04:33 +0100

    Implémentation de la météo.

--- tasks/src/environments/environment.prod.ts
+++ tasks/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiKey: 'd31323e1c605c6ae6151098d6b52fa68'
+
 };
--- tasks/src/environments/environment.ts
+++ tasks/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiKey: 'd31323e1c605c6ae6151098d6b52fa68'
+
 };
 
 /*
