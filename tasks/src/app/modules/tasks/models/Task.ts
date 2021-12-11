b5f342d66b375219103cde96cb60a82df1e38d20
Kamil.ABDEL-KADER@etu.uca.fr <kamil.abdel-kader@etu.uca.fr>
2021-12-11 10:40:36 +0100

    Modifications classe Task et TaskService

--- tasks/src/app/modules/tasks/models/Task.ts
+++ tasks/src/app/modules/tasks/models/Task.ts
@@ -5,6 +5,7 @@ export class Task {
     private readonly id: string,
     public title: string,
     public description: string,
+    public date: string,
     public level: TaskLevel,
     public closed: boolean,
   ){
