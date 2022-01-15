bb6c83d60daf13b86bc5548e7e9f6c362347e918
kamil6311 <kamil63000@gmail.com>
2022-01-15 11:21:45 +0100

    modif local strategy

--- todo-app-cloud/src/auth/local.strategy.ts
+++ todo-app-cloud/src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { AuthService } from "./auth.service";
@@ -13,10 +13,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     public async validate(username: string, password: string): Promise<string> {
 
         const loginMessage = await this._authService.validateUser(username, password);
-        
-        if(!loginMessage){
-            throw new UnauthorizedException();
-        }   
         
         return loginMessage;
     }
