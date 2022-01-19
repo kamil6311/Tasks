6d8ebe531c2b203f5d2acef58f55c5b5a0803412
kamil6311 <kamil63000@gmail.com>
2022-01-19 22:21:32 +0100

    Heroku deployment.

--- todo-app-cloud/todo-api-node-nest/src/auth/jwt.strategy.ts
+++ todo-app-cloud/todo-api-node-nest/src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private _configService: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false, //TODO add verify token is still valid 
+            ignoreExpiration: true, //TODO add verify token is still valid 
             secretOrKey: _configService.get('JWT_SECRET') 
         })
     }
